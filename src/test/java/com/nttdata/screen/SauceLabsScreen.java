68cec1f7257a5d16338722b6b04827e1b302d719
lvallash <leticiavh_3@hotmail.com>
2024-08-22 20:49:53 -0500

    tercer commit

--- src/test/java/com/nttdata/screen/SauceLabsScreen.java
+++ src/test/java/com/nttdata/screen/SauceLabsScreen.java
@@ -2,16 +2,15 @@ package com.nttdata.screen;
 
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import net.serenitybdd.core.pages.PageObject;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
-import java.util.logging.Logger;
 
 public class SauceLabsScreen extends PageObject {
-    private static final Logger LOGGER = Logger.getLogger(SauceLabsScreen.class.getName());
-    private static final int TIMEOUT = 20;
 
     @AndroidFindBy(xpath = "//androidx.recyclerview.widget.RecyclerView[@content-desc='Displays all products of catalog']//android.view.ViewGroup")
     private List<WebElement> productos;
@@ -40,42 +39,41 @@ public class SauceLabsScreen extends PageObject {
     @AndroidFindBy(xpath = "//android.widget.TextView[@resource-id=\"com.saucelabs.mydemoapp.android:id/itemsTV\"]")
     private WebElement totalProds;
 
-    private WebDriverWait wait;
-
-    public SauceLabsScreen() {
-        wait = new WebDriverWait(getDriver(), TIMEOUT);
-    }
-
-    private void waitVisibility(WebElement element) {
-        wait.until(ExpectedConditions.visibilityOf(element));
-    }
-
-    private void waitProdsVisibility(List<WebElement> elements) {
-        wait.until(ExpectedConditions.visibilityOfAllElements(elements));
+    private WebDriverWait getWait() {
+        return new WebDriverWait(getDriver(), Duration.ofSeconds(10)); // Timeout en segundos
     }
 
     public void prodsQuantity() {
-        waitVisibility(productos.get(0));
-        LOGGER.info("Cantidad en stock: " + productos.size());
+        WebDriverWait wait = getWait();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//androidx.recyclerview.widget.RecyclerView[@content-desc='Displays all products of catalog']//android.view.ViewGroup")));
+        int quantity = productos.size();
+        System.out.println("Cantidad en stock (int): " + quantity);
     }
 
+
     public int validateLoading() {
-        waitProdsVisibility(productos);
+        WebDriverWait wait = getWait();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//androidx.recyclerview.widget.RecyclerView[@content-desc='Displays all products of catalog']//android.view.ViewGroup")));
         return productos.size();
     }
 
     public void selectProds(String nomProd, int cant) {
+        WebDriverWait wait = getWait();
         WebElement producto = getProdsByName(nomProd);
-        waitVisibility(producto);
+        wait.until(ExpectedConditions.visibilityOf(producto));
         producto.click();
-        increaseProductQuantity(cant);
+
+        wait.until(ExpectedConditions.visibilityOf(btnAumentar));
+        for (int i = 1; i < cant; i++) {
+            btnAumentar.click();
+        }
     }
 
     private WebElement getProdsByName(String nomProd) {
         switch (nomProd) {
             case "Sauce Labs Backpack":
                 return prod1;
-            case "Sauce Labs Bolt - T-Shirt":
+            case "Sauce Labs Bolt T-Shirt":
                 return prod2;
             case "Sauce Labs Bike Light":
                 return prod3;
@@ -84,30 +82,27 @@ public class SauceLabsScreen extends PageObject {
         }
     }
 
-    private void increaseProductQuantity(int cant) {
-        waitVisibility(btnAumentar);
-        for (int i = 1; i < cant; i++) {
-            btnAumentar.click();
-        }
-    }
-
     public void addToCart() {
-        waitVisibility(btnAgregar);
+        WebDriverWait wait = getWait();
+        wait.until(ExpectedConditions.visibilityOf(btnAgregar));
         btnAgregar.click();
     }
 
     public void goToCart() {
-        waitVisibility(btnCarrito);
+        WebDriverWait wait = getWait();
+        wait.until(ExpectedConditions.visibilityOf(btnCarrito));
         btnCarrito.click();
     }
 
     public int confirmAmount() {
-        waitProdsVisibility(visualizarProds);
+        WebDriverWait wait = getWait();
+        wait.until(ExpectedConditions.visibilityOfAllElements(visualizarProds));
         return visualizarProds.size();
     }
 
     public void totalProd() {
-        waitVisibility(totalProds);
-        LOGGER.info("Total de productos: " + totalProds.getText());
+        WebDriverWait wait = getWait();
+        wait.until(ExpectedConditions.visibilityOf(totalProds));
+        System.out.println("Total de productos: " + totalProds.getText());
     }
 }
